071aa35b596133d5a080a3cd47758cad5daf32d7
Salipha <92833183+Salipha@users.noreply.github.com>
2022-04-06 20:44:13 -0400

    all

---
+++ client/build/precache-manifest.1b2dfc450dcc733fd4a22d05baf28a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0367d962be733e88d95e2ec14acba778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209153fbd658f33b43d2",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "ebee31ce927c4b970ddf",
+    "url": "/static/js/2.53c7959c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.53c7959c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209153fbd658f33b43d2",
+    "url": "/static/js/main.f103aec1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
