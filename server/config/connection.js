24e1f936753d6c5acd1f111cf75b9805499fb647
msoht <msoht@yahoo.com>
2022-04-13 21:58:03 -0400

    mongoodse

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
+   
+ 
 });
 
 module.exports = mongoose.connection;
